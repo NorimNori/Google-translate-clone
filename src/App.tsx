c538b6124fac258c23d48c5c8269d9a145bafafa
Gloria Rangel <norimaqidoe@hotmail.com>
2024-05-21 22:30:59 -0600

    Adding some bootstrap components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 import { useStore } from './assets/hooks/useStore';
-import { Container, Row, Col, Button } from 'react-bootstrap'
+import { Container, Row, Col, Button, Form, Stack } from 'react-bootstrap'
 import { AUTO_LANGUAGE } from './assets/constants';
 import { ArrowIcon } from './assets/components/Icons';
 import { LanguageSelector } from './assets/components/LanguageSelector';
+import { SectionType } from './types.d';
 
 function App() {
     const { fromLanguage, toLanguage, interchangeLaguages, setFromLanguage, setToLanguage } = useStore()
@@ -12,32 +13,46 @@ function App() {
     return (
         <>
             <Container fluid>
+                <h2>Google translate</h2>
 
                 <Row>
                     <Col>
-                        <LanguageSelector onChange={setFromLanguage}
-                            type='from'
-                            value={fromLanguage}
-                        />
-                        {fromLanguage}
+                        <Stack gap={2}>
+                            <LanguageSelector onChange={setFromLanguage}
+                                type={SectionType.From}
+                                value={fromLanguage}
+                            />
+                            <Form.Control
+                                as='textarea'
+                                placeholder='Introducir texto'
+                                autoFocus
+                                style={{ height:'150px'}}
+                            />
+                        </Stack>
                     </Col>
 
-                    <Col>
+                    <Col xs='auto'>
                         <Button variant='link' disabled= {fromLanguage === AUTO_LANGUAGE} onClick={interchangeLaguages}>
                             <ArrowIcon />
                         </Button>
                     </Col>
 
                     <Col>
-                        <LanguageSelector 
-                            type='to'
-                            value={toLanguage}
-                            onChange={setToLanguage}
-                        />
-                        {toLanguage}
+                        <Stack gap={2}>
+                            <LanguageSelector 
+                                type={SectionType.To}
+                                value={toLanguage}
+                                onChange={setToLanguage}
+                            />
+                            <Form.Control
+                                as='textarea'
+                                placeholder='Traduccion'
+                                style={{ height:'150px'}}
+                            />
+                        </Stack>
                     </Col>
                 </Row>
-                <h1>Google translate</h1>
+
             </Container>
         </>
     )
