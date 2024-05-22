13e782a9f9262b6e6952c132fe26231d397fbe57
Gloria Rangel <norimaqidoe@hotmail.com>
2024-05-21 22:09:54 -0600

    Adding functionality to switch and select language

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,44 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 import { useStore } from './assets/hooks/useStore';
+import { Container, Row, Col, Button } from 'react-bootstrap'
+import { AUTO_LANGUAGE } from './assets/constants';
+import { ArrowIcon } from './assets/components/Icons';
+import { LanguageSelector } from './assets/components/LanguageSelector';
 
 function App() {
-    const { fromLanguage, setFromLanguage } = useStore()
+    const { fromLanguage, toLanguage, interchangeLaguages, setFromLanguage, setToLanguage } = useStore()
 
     return (
         <>
-            <div className='app'>
+            <Container fluid>
+
+                <Row>
+                    <Col>
+                        <LanguageSelector onChange={setFromLanguage}
+                            type='from'
+                            value={fromLanguage}
+                        />
+                        {fromLanguage}
+                    </Col>
+
+                    <Col>
+                        <Button variant='link' disabled= {fromLanguage === AUTO_LANGUAGE} onClick={interchangeLaguages}>
+                            <ArrowIcon />
+                        </Button>
+                    </Col>
+
+                    <Col>
+                        <LanguageSelector 
+                            type='to'
+                            value={toLanguage}
+                            onChange={setToLanguage}
+                        />
+                        {toLanguage}
+                    </Col>
+                </Row>
                 <h1>Google translate</h1>
-                <button onClick={() => {
-                    setFromLanguage( 'es' )
-                }}>Cambiar a Espanol</button>
-                {fromLanguage}
-            </div>
+            </Container>
         </>
     )
 }
